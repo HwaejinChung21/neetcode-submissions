b71f200c8021fe81995176067f3b4b8bb5f9e0ab
Hwaejin Chung <colechung@gmail.com>
2026-07-31 11:30:52 +0900

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,45 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+
+        sMap, tMap = {}, {}
+        l, r = 0, 0
+        res = [0, 0]
+        bestLength = float("inf")
+
+        for c in t:
+            tMap[c] = 1 + tMap.get(c, 0)
+
+        need = len(tMap)
+        have = 0
+
+        while r < len(s):
+            # skip over ones we don't need
+            if s[r] in tMap:
+                sMap[s[r]] = 1 + sMap.get(s[r], 0)
+
+                if sMap[s[r]] == tMap.get(s[r], 0):
+                    have += 1
+
+            while have == need:
+                curLength = r - l + 1
+
+                if curLength < bestLength:
+                    bestLength = curLength
+                    res = [l, r]
+
+                if s[l] in tMap:
+                    sMap[s[l]] -= 1
+
+                    if sMap[s[l]] < tMap[s[l]]:
+                        have -= 1
+            
+                l += 1
+            
+            r += 1
+
+        left = res[0]
+        right = res[1]
+
+        return "" if bestLength == float("inf") else s[left:right + 1]
